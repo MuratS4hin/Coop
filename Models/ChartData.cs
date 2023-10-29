d68ee50668b00ec50ce18e924683e3c230476968
Murat <sahin.mur4t@gmail.com>
2023-10-29 10:24:49 +0300

    Model updated and Informations added to view

--- Models/ChartData.cs
+++ Models/ChartData.cs
@@ -4,6 +4,11 @@ public class ChartData
 {
     public List<string> Labels { get; set; }
     public List<int> Values { get; set; }
+    public int MaxPopulation { get; set; }
+    public int TotalMaleNumber { get; set; }
+    public int MaxMaleAge { get; set; }
+    public int TotalFemaleNumber { get; set; }
+    public int MaxFemaleAge { get; set; }
 
     public ChartData()
     {
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,28 +9,39 @@ public class IndexModel : PageModel
 {
     [BindProperty]
     public string Loop { get; set; }
-    public List<Animals> animals { get; set; }
+    public List<Animals> Animals { get; set; }
     private ISimulation _simulation { get; set; }
     public ChartData ChartData { get; set; }
     public bool ShowForm { get; set; }
 
     public IndexModel(ISimulation simulation)
     {
-        _simulation = simulation;
-        animals = new List<Animals>
+        Animals = new List<Animals>
         {
             new Animals { Id = 1, Species = "Rabbit", Sex = "Male", Age = 1 , IsPragnent = false },
             new Animals { Id = 2, Species = "Rabbit", Sex = "Female", Age = 1, IsPragnent = false },
         };
-        ChartData = new ChartData();
+        _simulation = simulation;
         ShowForm = true;
+
+        #region Set ChartData
+        int totalAnimalCount = Animals.Count();
+        int maleAge = (int)Animals.FindAll(x => x.Sex == "Male")?.MaxBy(t => t.Age).Age;
+        int femaleAge = (int)(Animals.FindAll(x => x.Sex == "Female")?.MaxBy(t => t.Age).Age);
+        ChartData = new ChartData();
+        ChartData.MaxPopulation = ChartData.MaxPopulation > totalAnimalCount ? ChartData.MaxPopulation : totalAnimalCount;
+        ChartData.MaxMaleAge = ChartData.MaxMaleAge > maleAge ? ChartData.MaxMaleAge : maleAge;
+        ChartData.MaxFemaleAge = ChartData.MaxFemaleAge > femaleAge ? ChartData.MaxFemaleAge : femaleAge;
+        ChartData.TotalMaleNumber = Animals.FindAll(x => x.Sex == "Male").Count();
+        ChartData.TotalFemaleNumber = Animals.FindAll(x => x.Sex == "Female").Count();
+        #endregion        
     }
 
     public IActionResult OnPost(string action)
     {        
         if(action == "LoopNumber")
         {
-            TempData["LoopValue"] = Loop; //Preserves the Input Value for regenerating
+            TempData["LoopValue"] = Loop; //Preserves the Input Value for regenerating            
             ShowForm = false;
             StartSimulation();
         }
@@ -56,16 +67,32 @@ public class IndexModel : PageModel
             Loop = "1";
         int counter = 0;
         ChartData.Labels.Add(counter.ToString());
-        ChartData.Values.Add(animals.Count());
+        ChartData.Values.Add(Animals.Count());
 
         while (true)
         {
-            IEnumerable<IGrouping<string, Animals>> groupedAnimals = animals.GroupBy(animal => animal.Species);
-            animals = _simulation.Loop(animals, groupedAnimals);
+            IEnumerable<IGrouping<string, Animals>> groupedAnimals = Animals.GroupBy(animal => animal.Species);
+            Animals = _simulation.Loop(Animals, groupedAnimals);
             counter++;
+
+            #region Set ChartData
+            int totalAnimalCount = Animals.Count();
+            int maleAge = Animals?.FindAll(x => x.Sex == "Male").MaxBy(t => t.Age)?.Age ?? ChartData.MaxMaleAge;
+            int femaleAge = Animals?.FindAll(x => x.Sex == "Female").MaxBy(t => t.Age)?.Age ?? ChartData.MaxFemaleAge;
             ChartData.Labels.Add(counter.ToString());
-            ChartData.Values.Add(animals.Count());
-            if (counter == Int32.Parse(Loop)) break;
+            ChartData.Values.Add(totalAnimalCount);
+            ChartData.MaxPopulation = ChartData.MaxPopulation > totalAnimalCount ? ChartData.MaxPopulation : totalAnimalCount;
+            ChartData.MaxMaleAge = ChartData.MaxMaleAge > maleAge ? ChartData.MaxMaleAge : maleAge;
+            ChartData.MaxFemaleAge = ChartData.MaxFemaleAge > femaleAge ? ChartData.MaxFemaleAge : femaleAge;
+            #endregion
+
+            if (counter == Int32.Parse(Loop))
+                break;
         }
+
+        #region Set ChartData
+        ChartData.TotalMaleNumber = Animals.FindAll(x => x.Sex == "Male").Count();
+        ChartData.TotalFemaleNumber = Animals.FindAll(x => x.Sex == "Female").Count();
+        #endregion
     }
 }
